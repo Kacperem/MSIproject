53d11180e32d1c3d086ca8050997aef665bad2ce
Kacperem <kacp.rembisz@gmail.com>
2020-06-01 18:52:59 +0200

    add markers

--- frontend/src/components/map/map.js
+++ frontend/src/components/map/map.js
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
-import ReactMapGL, { GeolocateControl } from "react-map-gl";
+import ReactMapGL, { GeolocateControl, Marker, Popup } from "react-map-gl";
 
+const markers = [
+  {
+    id: 7,
+    name: "test patch 4",
+    latitude: "51.2540",
+    longitude: "17.3690",
+    description: "ala ma kota i nie ma psa",
+    image: "",
+  },
+];
 const TOKEN =
   "pk.eyJ1Ijoia2FjcGVycmVtIiwiYSI6ImNrYXZpZHczczNsNHIzMXA2eGZjZjNwMXUifQ.GlqVRBjPz0cNx-KBRGKg6w";
 
@@ -12,7 +22,7 @@ const geolocateStyle = {
 };
 
 export default function map() {
-  const [viewport, setViewport] = useState({
+  const [viewport, setViewport, selectedMarker, setSelectedMarker] = useState({
     latitude: 51.1088,
     longitude: 17.0582,
     width: "100vw",
@@ -30,6 +40,38 @@ export default function map() {
           setViewport(viewport);
         }}
       >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.id}
+            latitude={marker.latitude}
+            longitude={marker.longitude}
+          >
+            <button
+              className="marker-btn"
+              onClick={(e) => {
+                e.preventDefault();
+                setSelectedMarker(marker);
+              }}
+            >
+              <img src="/skateboarding.svg" alt="Skate Park Icon" />
+            </button>
+          </Marker>
+        ))}
+
+        {selectedMarker ? (
+          <Popup
+            latitude={selectedMarker.latitude}
+            longitude={selectedMarker.longitude}
+            onClose={() => {
+              setSelectedMarker(null);
+            }}
+          >
+            <div>
+              <h2>{selectedMarker.name}</h2>
+              <p>{selectedMarker.description}</p>
+            </div>
+          </Popup>
+        ) : null}
         <GeolocateControl
           style={geolocateStyle}
           positionOptions={{ enableHighAccuracy: true }}
